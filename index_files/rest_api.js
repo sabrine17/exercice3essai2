7d2728f1251fc401a0a47c32743940310574f1b1
sabrine17 <sabrine.cheurfa@yahoo.ca>
2021-04-26 22:36:52 -0400

    modification read me

---
+++ index_files/rest_api.js
@@ -0,0 +1,34 @@
+
+( function monAjax( ) {
+
+	//let bouton = document.getElementById('bout_nouvelles');
+	let nouvelles = document.querySelector('.nouvelles section');
+	//console.log(bouton.id);
+
+	//bouton.addEventListener('mousedown', monAjax);
+	window.addEventListener('load', monAjax);
+	let maRequete = new XMLHttpRequest();
+   	console.log(maRequete);
+  	maRequete.open('GET', 'http://localhost:8888/4w4/wp-json/wp/v2/posts');
+    maRequete.onload = function () {
+       console.log(maRequete);
+       if (maRequete.status >= 200 && maRequete.status < 400) {
+           let data = JSON.parse(maRequete.responseText);
+		   let chaine = ''
+		   for (const elm of data){
+			   chaine += '<h2>' + elm.title.rendered + '</h2>';
+			   chaine +=  elm.content.rendered;
+		   }
+		   nouvelles.innerHTML = maChaine;
+		}
+      
+        else {
+           console.log('La connexion est faite mais il y a une erreur')
+       }
+   }
+   maRequete.onerror = function () {
+       console.log("erreur de connexion");
+   }
+   maRequete.send() 
+}( ) )
+
