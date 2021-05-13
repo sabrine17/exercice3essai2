45b6f06201d5c7d9ee7ea0e1fb916c0fca6e1a84
sabrine17 <sabrine.cheurfa@yahoo.ca>
2021-05-13 18:38:16 -0400

    index files

--- README.md
+++ README.md
@@ -5,8 +5,7 @@ Auteur: Sabrine Cheurfa
 Groupe 2
 
 Description du site :
-Mon site est un site s'adressant principalement aux étudiants et nouveaux étudiants qui s'intéresse au programme TIM il résume les cours et une description de chaques cours donnés dans la technique. De plus, je 
-voulais adapter mon site avec des images d'animes pour cibler une clientèle plus jeune. Je voulais que le design de mon site soit simple et épuré.
+Mon site est un site s'adressant principalement aux étudiants et nouveaux étudiants qui s'intéresse au programme TIM il résume les cours et une description de chaques cours donnés dans la technique. De plus, je voulais adapter mon site avec des images d'animes pour cibler une clientèle plus jeune. Je voulais que le design de mon site soit simple et épuré.
 
 Le titre de mon site : TIM 
 
---
+++ index_files/rest_api.js
@@ -0,0 +1,74 @@
+(function(){
+
+
+	//let bouton = document.getElementById('bout_nouvelles');
+	let nouvelles = document.querySelector('.nouvelles');
+	//console.log(bouton.id);
+    
+	//bouton.addEventListener('mousedown', monAjax);
+	window.addEventListener('load', monAjax);
+
+ function monAjax() {
+
+	let maRequete = new XMLHttpRequest();
+   	console.log(maRequete);
+  	maRequete.open('GET', monObjJS.URLDomaine  + '/wp-json/wp/v2/posts?categories=33');
+    maRequete.onload = function () {
+       console.log(maRequete);
+       if (maRequete.status >= 200 && maRequete.status < 400) {
+           let data = JSON.parse(maRequete.responseText);
+		   let chaine = ''
+		   for (const elm of data){
+			   chaine += '<h2>' + elm.title.rendered + '</h2>'
+			   chaine +=  elm.content.rendered;
+		   }
+		   nouvelles.innerHTML = chaine;
+		}
+      
+        else {
+           console.log('La connexion est faite mais il y a une erreur')
+       }
+   }
+   maRequete.onerror = function () {
+       console.log("erreur de connexion");
+   }
+   maRequete.send() 
+
+
+}
+/* ----------------------------------------------
+Traitement de l'ajout d'un article de cat Nouvelles
+------------------------------------------------*/
+
+	bouton_ajout = document.getElementById('bout-rapide')
+	bouton_ajout.addEventListener('mousedown', function(){
+	console.log('ajout')
+	let = monArticle = {
+		"title" : document.querySelector('.admin-rapid [name="title"]').value,
+		"content" : document.querySelector('.admin-rapid [name="content"]').value,
+		"status" : "publish",
+		"categories" : [33]
+	}
+
+	console.log(JSON.stringify(monArticle))
+	let creerArticle = new XMLHttpRequest()
+	creerArticle.open("POST", monObjJS.URLDomaine  + '/wp-json/wp/v2/posts' )
+	creerArticle.setRequestHeader("X-WP-Nonce", monObjJS.nonce)
+	creerArticle.setRequestHeader("Content-Type", "application/json;charset=UTF8-8")
+	creerArticle.send(JSON.stringify(monArticle)) //transmettre la requete au serveur
+	creerArticle.onreadystatechange = function(){
+		if(creerArticle.readyState == 4){
+			if(creerArticle.status == 201){
+				document.querySelector('.admin-rapid [name="title"]').value	=''
+				document.querySelector('.admin-rapid [name="content"]').value =''
+			}
+			else{
+				alert('Erreur réessayez')
+			}
+		}
+	}
+})
+
+
+}( ) )
+
